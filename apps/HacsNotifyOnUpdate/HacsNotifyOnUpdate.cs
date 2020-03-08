d482b095b8866a6b96599e98019c22408a8098c9
ludeeus <joasoe@gmail.com>
2020-03-08 19:37:34 +0100

    Fix service call

--- apps/HacsNotifyOnUpdate/HacsNotifyOnUpdate.cs
+++ apps/HacsNotifyOnUpdate/HacsNotifyOnUpdate.cs
@@ -2,20 +2,15 @@ using System.Threading.Tasks;
 using JoySoftware.HomeAssistant.NetDaemon.Common;
 public class HacsNotifyOnUpdate : NetDaemonApp
 {
-    public async override Task InitializeAsync()
-    {
-        Entity("sensor.hacs")
+    public async override Task InitializeAsync() => Entity("sensor.hacs")
             .WhenStateChange((n, o) =>
-                o?.State != n?.State )
+                o?.State != n?.State && n?.State > 0)
                     .Call(async (entityId, newState, oldState) =>
                     {
-                        var serviceData = new 
+                        await CallService("persistent_notification", "create", new
                         {
                             title = "Updates pending in HACS",
                             message = "There are updates pending in [HACS](/hacs)"
-                        };
-                        await CallService("persistent_notification", "create ", serviceData);
+                        }, true);
                     }).Execute();
-
-    }
 }
